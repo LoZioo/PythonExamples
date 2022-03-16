94e2def32e6d9ed8c870046c36f93d04942b7a18
LoZioo <60042676+LoZioo@users.noreply.github.com>
2022-03-16 12:59:40 +0100

    Modificato il file var_types.py

--- var_types.py
+++ var_types.py
@@ -1,3 +1,5 @@
+# https://www.w3schools.com/python/python_datatypes.asp
+
 # Python has the following data types built-in by default, in these categories:
 # Text Type:	str
 # Numeric Types:	int, float, complex
@@ -7,7 +9,22 @@
 # Boolean Type:	bool
 # Binary Types:	bytes, bytearray, memoryview
 
-# Vedere come stampare complesso...
-# https://www.w3schools.com/python/python_datatypes.asp
 z = 3 + 4j
-print("z: %f, type(z): %s" % (z, type(z)))
+print("z: %f %fj, type(z): %s" % (z.real, z.imag, type(z)))
+
+# set
+x = {"apple", "banana", "cherry"}
+print(x)
+
+# dict (o json)
+x = dict(name="John", age=36)
+x = {"name": "John", "age": 36}
+print(x)
+
+# tuple (immutabile)
+x = ("apple", "banana", "cherry", 123)
+print(x)
+
+# list (o array) (mutabile)
+x = ["apple", "banana", "cherry", 123]
+print(x)
